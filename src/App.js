19b0c30168bd0826729668974222dde54c02c41c
vedantpawar004 <pawarvedu004@gmail.com>
2024-04-13 14:18:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import React, { useContext } from "react";
+import { Route,Routes, Switch ,Navigate} from "react-router-dom";
+import Login from './pages/login/Login';
+import Regiser from './pages/register/Register';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import UpdateUser from "./component/updateUser/UpdateUser";
+import Comment from './component/comment/Comment';
+import UpdatePost from "./component/updatepost/UpdatePost";
+import Messenger from "./pages/messenger/messenger";
+import {useSelector} from 'react-redux'
+import SearchUserPage from "./pages/searchUserPage/SearchUserPage";
 function App() {
+  const {currentUser:user}=useSelector((state)=>state.user)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+     <Route path="/"  exact element={user?<Home />:<Regiser />} />
+     <Route path="/login"  element={user?<Navigate to="/" />:<Login />} />
+     <Route path="/register"  element={user?<Navigate to="/" />:<Regiser />} />
+     <Route path="/messenger" element={!user ? <Navigate to="/" /> : <Messenger />} />
+     <Route path="/profile/:userId"   element={<Profile />} />
+     <Route path="/searchuser"   element={<SearchUserPage />} />
+     <Route path="/profile/update"   element={<UpdateUser />} />
+     <Route path="/note/update/:notesid"   element={<UpdatePost />} />
+     <Route path="/viewcomment/:notesid"   element={<Comment />} />
+     </Routes>
+    </> 
   );
 }
 
---
+++ src/requestMethods.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const LocalBASE_URL = "http://localhost:8000/api/";
+// const BASE_URL = "https://notesharingbackend-ankitkr437.onrender.com/api/";
+
+export const pf =
+  "https://notesharingbackend-ankitkr437.onrender.com/images";
+
+export const publicRequest = axios.create({
+  baseURL: LocalBASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: LocalBASE_URL,
+});
